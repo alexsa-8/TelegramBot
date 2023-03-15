32b9cae8d3a4cd491b1eba2ad3b299daa749267d
Alex <alexsa-8@mail.ru>
2023-03-15 20:34:51 +0300

    TelegramBot

--- src/main/java/com/example/telegrambot/entity/NotificationTask.java
+++ src/main/java/com/example/telegrambot/entity/NotificationTask.java
@@ -1,4 +1,12 @@
 package com.example.telegrambot.entity;
 
 public class NotificationTask {
+    private Integer id;
+
+
+    private Long chatId;
+
+    private String message;
+
+    //private LocalDateTime time;
 }
